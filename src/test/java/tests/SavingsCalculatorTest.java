163c53285fd3d3abbdc9d59cfac3bf334604257c
Peter Steyr <e-Peter.Steyr@embedit.cz>
2019-07-29 12:49:18 +0200

    add test, remove comments

--- src/test/java/tests/SavingsCalculatorTest.java
+++ src/test/java/tests/SavingsCalculatorTest.java
@@ -55,7 +55,7 @@ public class SavingsCalculatorTest {
 
     @After
     public void tearDown() {
-//        driver.close();
-//        driver.quit();
+        driver.close();
+        driver.quit();
     }
 }
