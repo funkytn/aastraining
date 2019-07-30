09633e08a1e46a7c8ff373af6ad61dc14910bd74
Peter Steyr <e-Peter.Steyr@embedit.cz>
2019-07-30 16:06:22 +0200

    updated DefaultSuite

--- src/test/java/suites/DefaultSuite.java
+++ src/test/java/suites/DefaultSuite.java
@@ -5,12 +5,18 @@ import org.junit.runners.Suite;
 
 import tests.CalculationTest;
 import tests.SavingsCalculatorTest;
+import tests.NewSavingRequestTest;
+import tests.EmailValidationTest;
+
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 
     CalculationTest.class,
-    SavingsCalculatorTest.class
+    SavingsCalculatorTest.class,
+    NewSavingRequestTest.class,
+    EmailValidationTest.class,
+
 
 
 })
