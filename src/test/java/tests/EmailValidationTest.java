1f823bcdd0965e2f6d8d6e9a377932c36080dfda
Peter Steyr <e-Peter.Steyr@embedit.cz>
2019-07-30 16:00:57 +0200

    check invalid emails form txt file

--- src/test/java/tests/EmailValidationTest.java
+++ src/test/java/tests/EmailValidationTest.java
@@ -6,13 +6,33 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import pages.CalculatorPage;
 
+import javax.print.DocFlavor;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EmailValidationTest extends TestBase {
 
     @Test
-    public void itShouldDisplayErrorWhenEmailIsInvalid() {
+    public void itShouldDisplayErrorWhenEmailIsInvalid() throws FileNotFoundException {
         CalculatorPage calculatorPage = new CalculatorPage(driver);
-        calculatorPage.selectEmail("asfasfsadf");
-        Assert.assertTrue(driver.findElement(By.xpath("//div[input[@id='emailInput']]")).getAttribute("class").contains("error"));
+        //zadam zly email
+        for (String invalidEmail : readInvalidEmails()) {
+            calculatorPage.selectEmail(invalidEmail);
+            Assert.assertTrue(driver.findElement(By.xpath("//div[input[@id='emailInput']]"))
+                    .getAttribute("class")
+                    .contains("error"));
+        }
+    }
 
+    private List<String> readInvalidEmails() throws FileNotFoundException {
+        FileReader invalidEmailsTxt = new FileReader(new File("src/test/resources/invalid_emails.txt"));
+        List<String> invalidEmailList = new ArrayList<>();
+        invalidEmailList = new BufferedReader(invalidEmailsTxt).lines().collect(Collectors.toList());
+        return invalidEmailList;
     }
 }
